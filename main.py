a97dc6960e7663f9a415454db9080682dcc80499
Ivan Ryzhov <faiser1337@mail.ru>
2022-07-01 16:29:32 +0300

    commit

--- main.py
+++ main.py
@@ -1,10 +1,19 @@
-def cego(i):
-    if i in range(1,10):
-        print("all good")
+def cego(i,j):
+    S = 0
+    if i in range(1,10) and j in range(1,10):
+        S = i + j
+        print("i + j = ", S)
+
+    else:
+        print("всем пока")
 
 
 
 
 
-a = int(input())
-cego(a)
+
+
+a = int(input("Введите i :"))
+b = int(input("Введите j :"))
+
+cego(a,b)
