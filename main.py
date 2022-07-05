4167d2694a7f7bed0d8423181a12c50445a30925
Ivan Ryzhov <faiser1337@mail.ru>
2022-07-05 23:17:44 +0300

    программа с функцией.

--- main.py
+++ main.py
@@ -1,19 +1,22 @@
 def cego(i,j):
     S = 0
-    if i in range(1,10) and j in range(1,10):
-        S = i + j
-        print("i + j = ", S)
+    if i in range(1,7) and j in range(1,4):
+        S = pow(i,j)
+        print(S)
 
+    elif i in range(8,20) and j in range(8,20):
+        S = i - j
+        print("|i - j| = ", abs(S))
     else:
-        print("всем пока")
-
-
-#cdfdfldfld
-
-
+        print("ощибочка")
 
 
 a = int(input("Введите i :"))
 b = int(input("Введите j :"))
 
 cego(a,b)
+
+
+
+
+
