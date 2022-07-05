c712275b39ad02d30133c722676305ff8461ed03
Ivan Ryzhov <faiser1337@mail.ru>
2022-07-05 23:09:12 +0300

    Делал все то что было во втором уроке(почти все).

---
+++ lesson 2.py
@@ -0,0 +1,21 @@
+x = int(input("Введите значение 1 :"))
+y = int(input("Введите значение 2 :"))
+
+print(x + y)
+print(x - y)
+print(x * y)
+print(x / y)
+print(x // y)
+print(x ** y)
+print(x ** 0.5)
+print(x % y)
+
+print()
+
+print(12 ** 2 - 100 + 20 % 12)
+
+print()
+
+
+z = 12.123453
+print(round(z),round(z,3))
